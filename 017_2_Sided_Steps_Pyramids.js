25bae9b849e1e4e687ff89964833d9d6d8e3dd74
Christian Anderson <ca96187@gmail.com>
2019-10-05 16:41:12 -0600

    completed solution

--- 017_2_Sided_Steps_Pyramids.js
+++ 017_2_Sided_Steps_Pyramids.js
@@ -12,12 +12,32 @@
 
 // --Logic
 // From 0 to n (iterate through rows)
-  // Create an empty string, 'level'
-  // From 0 to ??? (columns)
-    // IF the column should have a #
-      // Add a '#' to 'level'
-    // ELSE
-      // Add a space to 'level'
+// Create an empty string, 'level'
+// From 0 to ??? (columns)
+// IF the column should have a #
+// Add a '#' to 'level'
+// ELSE
+// Add a space to 'level'
 // console log 'stair'
 
 
+const pyramid = n => {
+
+  const midpoint = Math.floor((2 * n - 1) / 2)
+
+  for (let row = 0; row < n; row++) {
+    let level = ''
+
+    for (let column = 0; column < (2 * n - 1); column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#'
+      } else {
+        level += ''
+      }
+    }
+
+    console.log(level)
+  }
+}
+
+pyramid(3)

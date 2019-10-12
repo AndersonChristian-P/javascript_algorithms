7c62489672071c9262a8341fd260f50d8d0f8fef
Christian Anderson <ca96187@gmail.com>
2019-10-11 18:33:12 -0600

    forgot parens after reverse

--- 004_Reverse_String.js
+++ 004_Reverse_String.js
@@ -1,3 +1,5 @@
 // reverse the characters in a string
 
-const reverseString = str => str.split('').reverse.join('')
+const reverseString = str => str.split('').reverse().join('')
+
+console.log(reverseString("hello"))

7cf16047a9996036afef7b0b4181d08f7675b7c6
Christian Anderson <ca96187@gmail.com>
2019-10-13 15:06:24 -0600

    iterative solution

---
+++ 020_Runtime_Complexity_in_Fibonacci.js
@@ -0,0 +1,40 @@
+// -- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where each number
+// is the sum of the preceeding two.
+
+// For example, the sequence
+// [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first 10 entries of the fibonacci series.
+
+// -- Example
+//    fib(4) === 3
+
+
+// Iterative Solution
+const fib = n => {
+
+  const result = [0, 1]
+  // this is the trick, you will always have 0 and 1 in the fibonacci sequence
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1]
+    const b = result[i - 2]
+
+    result.push(a + b)
+  }
+
+  return result[result.length - 1]
+
+}
+
+// -- Logic
+  // create an array that will hold our series of numbers
+  // return the nth element
+
+// -- Runtime Complexity
+// Cerebral academic approach
+  // For every increase in n we have to calculate one additional number - this will never change - linear runtime
+
+// Shortcut way
+  // We have an iterative for loop that loops over a closed set - chances are we have linear runtime

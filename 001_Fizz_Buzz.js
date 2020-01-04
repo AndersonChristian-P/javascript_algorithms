3599475565b7ea9a38057c571904a988c73d358e
Christian Anderson <ca96187@gmail.com>
2020-01-04 12:53:46 -0700

    removed console.log in line 15 and added semi-colons

--- 001_Fizz_Buzz.js
+++ 001_Fizz_Buzz.js
@@ -1,15 +1,15 @@
 const fizzBuzz = num => {
   for (let i = 0; i < num + 1; i++) {
     if (i % 15 === 0) {
-      console.log(`Fizz Buzz`)
+      console.log(`Fizz Buzz`);
     } else if (i % 3 === 0) {
-      console.log(`Fizz`)
+      console.log(`Fizz`);
     } else if (i % 5 === 0) {
-      console.log(`Buzz`)
+      console.log(`Buzz`);
     } else {
-      console.log(i)
+      console.log(i);
     }
   }
 }
 
-console.log(fizzBuzz(26))
+fizzBuzz(26);

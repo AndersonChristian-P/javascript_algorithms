0ce7a6698c976d4062a4b82f0ba1e7a128019978
Christian Anderson <ca96187@gmail.com>
2020-01-04 13:05:27 -0700

    addressed the case where n is less than 0

--- 026_factorial.js
+++ 026_factorial.js
@@ -1,10 +1,14 @@
 // -- Directions
 
-// Calculate the factorial of any number
+// Calculate the factorial of a number
 
 
 const factorialize = num => {
   let factorialTotal = num;
+
+  if (num < 0) {
+    return "Please provide a number >= 0";
+  }
 
   if (num === 0) {
     return 1;
